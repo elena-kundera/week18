1e18c13fdd4fef536ad4b489c240507b981981d3
Елена <elena@MacBook-Air-Elena-2.local>
2022-12-24 23:28:42 +0300

    Добавила последнее задание

--- script.js
+++ script.js
@@ -81,14 +81,76 @@ function check() {
 
 
 
-// 3. Доработать сохранение комментариев из прошлого домашнего задания через веб-хранилище, дать возможность пользователю сохранять свое имя и аватарку при вводе и запоминать их.
-//     ⚠️ *Аватарку задаем в виде ссылки на картинку в интернете. Работу с файлами тут использовать не нужно.*
+// 3. Доработать сохранение комментариев из прошлого домашнего задания 
+//через веб-хранилище, дать возможность пользователю сохранять свое имя и 
+//аватарку при вводе и запоминать их.
+//     ⚠️ *Аватарку задаем в виде ссылки на картинку в интернете. Работу с 
+//файлами тут использовать не нужно.*
+
+const message = document.querySelector('#messageInput');
+const sendBtn = document.querySelector('#sendComment');
+const messageOut = document.querySelector('#messageContainer');
+const avatarContainer = document.querySelector('#avatarContainer');
+
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+let user = localStorage.getItem('userName');
+        if(user != null) {
+        document.querySelector("#inputName").value = user;
+    }
+
+let ava = localStorage.getItem('userAva');
+if(ava != null) {
+    document.querySelector("#inputAva").value = ava;
+}
+});
+
+function sendMessage(userName, comment) {
+
+    let avaSrc = localStorage.getItem('userAva');
+    let image = new Image();
+    image.src = avaSrc;
+    avatarContainer.append(image);
+    messageOut.innerHTML += `<span class="user">${userName}: </span><span>${comment}</span> </br>`;
+}
+
+
+function getInfo() {
+    let userAvatar = document.querySelector("#inputAva").value;
+    let userName = document.querySelector("#inputName").value;
+    let comment = message.value;
+
+    localStorage.setItem('userName', userName);
+    localStorage.setItem('userAva', userAvatar);
+
+sendMessage(userName, comment);
+}
+
+sendBtn.onclick = function () {
+
+
+    const messageIn = message.value;
+    
+    if (messageIn.includes('viagra')){
+            const messageCorrected = messageIn.replace(/viagra/ig, '***');
+            messageOut.innerHTML = messageCorrected;
+        }
+        else if (messageIn.includes('xxx')){
+            const messageCorrected1 = messageIn.replace(/xxx/ig, '***');
+            messageOut.innerHTML = messageCorrected1;
+        }
+        else {
+            getInfo()
+        }
+        }
 
 
 
 
 // 4. Сделать локальное хранилище заметок для себя любимой. 
 //Не забыть валидацию при добавлении заметки.
+//Значение инпута в массив, массив - в хранилище
 
 document.addEventListener("DOMContentLoaded", function (event) {
     let note = localStorage.getItem('note');
@@ -104,4 +166,4 @@ let myNotesArea = document.querySelector("#myNotes");
 btnSendNote.onclick = function() {
     localStorage.setItem('note', noteArea.value);
     myNotesArea.innerHTML = localStorage.getItem('note');
-    };
+    }
