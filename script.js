1e799eb07ef03d826bddceca8e6dd00681270b1a
Елена <elena@MacBook-Air-Elena-2.local>
2022-12-24 17:43:56 +0300

    Добавила второе задание

--- script.js
+++ script.js
@@ -2,6 +2,85 @@
 
 // 2. Доработать форму регистрации из прошлых заданий, сделав для нее валидацию через JS.
 
+
+let errors = [];
+function checkInput(input) {
+    let validity = input.validity;
+
+    if(validity.valueMissing)
+        {errors.push('Поле ' + input.placeholder + ' не заполнено');}
+    if (validity.patternMismatch) 
+		{ errors.push('Неверный формат заполнения'); }
+    if(validity.tooLong)
+        {errors.push('Придумайте пароль покороче');}
+    if(validity.tooShort)
+        {errors.push('Придумайте пароль подлиннее');}
+    if (password.value != checkPassword.value) 
+        {errors.push('Ваши пароли не совпадают');}
+   }
+function check() {
+    errors = [];
+    let inputs = document.querySelectorAll("input");
+
+    for (let input of inputs) {
+        checkInput(input)
+    }
+    document.querySelector('#welcomeMsg').innerHTML = errors.join('. <br>');
+
+}
+
+
+// const firstName = document.querySelector("#firstName");
+// const lastName = document.querySelector("#lastName");
+// const email = document.querySelector("#email");
+// const password = document.querySelector("#password");
+// const checkPassword = document.querySelector("#checkPassword");
+
+// function check()
+// {
+//     document.querySelector('#smthWrong_firstName').innerHTML = "";
+//     document.querySelector('#smthWrong_lastName').innerHTML = "";
+//     document.querySelector('#smthWrong_email').innerHTML = "";
+//     document.querySelector('#smthWrong_password').innerHTML = "";
+//     document.querySelector('#smthWrong_checkPassword').innerHTML = "";
+//     document.querySelector("#welcomeMsg").innerHTML = "";
+// if (firstName.value == '')
+// {document.querySelector('#smthWrong_firstName').innerHTML+= "Это обязательное поле";
+// }
+
+// if (lastName.value == '')
+// {document.querySelector('#smthWrong_lastName').innerHTML+= "Это обязательное поле";
+// }
+
+// if (email.value == '')
+// {document.querySelector('#smthWrong_email').innerHTML+= "Это обязательное поле";
+// }
+
+// if (password.value == '')
+// {document.querySelector('#smthWrong_password').innerHTML+= "Это обязательное поле";
+// }
+
+// if (checkPassword.value == '')
+// {document.querySelector('#smthWrong_checkPassword').innerHTML+= "Это обязательное поле";
+// }
+
+// if (password.value != checkPassword.value)
+// {document.querySelector('#smthWrong_checkPassword').innerHTML+= "Ваши пароли не совпадают";
+// }
+
+// else 
+// {
+//     document.querySelector("#welcomeMsg").innerHTML+= "Добро пожаловать в систему, " + firstName.value + "!!!";
+//     firstName.value = '';
+//     lastName.value = '';
+//     email.value = '';
+//     password.value = '';
+//     checkPassword.value = '';
+// }}
+
+
+
+
 // 3. Доработать сохранение комментариев из прошлого домашнего задания через веб-хранилище, дать возможность пользователю сохранять свое имя и аватарку при вводе и запоминать их.
 //     ⚠️ *Аватарку задаем в виде ссылки на картинку в интернете. Работу с файлами тут использовать не нужно.*
 
